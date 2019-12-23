cc6d19e12c807e15bdb0d20959eb19f32cd3836c
siddhantchavan <siddhantchavan15@gmail.com>
2019-12-23 17:38:04 +0530

    driver register form upload image and owner registration ui

---
+++ app/src/main/java/com/example/siddhant/loginui/Driver.java
@@ -0,0 +1,59 @@
+package com.example.siddhant.loginui;
+
+public class Driver {
+    private String name;
+    private String address;
+    private String mobileno;
+    private String rtobranch;
+    private String rtoaddress;
+    private String dateofregistration;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    public String getRtobranch() {
+        return rtobranch;
+    }
+
+    public void setRtobranch(String rtobranch) {
+        this.rtobranch = rtobranch;
+    }
+
+    public String getRtoaddress() {
+        return rtoaddress;
+    }
+
+    public void setRtoaddress(String rtoaddress) {
+        this.rtoaddress = rtoaddress;
+    }
+
+    public String getDateofregistration() {
+        return dateofregistration;
+    }
+
+    public void setDateofregistration(String dateofregistration) {
+        this.dateofregistration = dateofregistration;
+    }
+
+}
--- app/src/main/java/com/example/siddhant/loginui/sign_up.java
+++ app/src/main/java/com/example/siddhant/loginui/sign_up.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
